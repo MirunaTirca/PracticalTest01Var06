2ed4c9376d3e5082067809a36cda01cb9c164d58
MirunaTirca <miruna.dalia@gmail.com>
2022-04-11 10:23:46 +0300

    solved A1+A2

---
+++ PracticalTest01Var06/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/view/PracticalTest01Var06MainActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.practicaltest01var06.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ro.pub.cs.systems.eim.practicaltest01var06.R;
+
+public class PracticalTest01Var06MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var06_main);
+    }
+}
